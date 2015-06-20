3eba9fdc6a015791686f46fe788bf27667c9cecb
安田裕介 <yyusuke@trifort.jp>
2015-06-20 15:06:21 +0900

    add addons: ofxMSAInteractiveObject, ofxQuadWarp generate project pingpong

---
+++ of_v0.8.4_osx_release/libs/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofMath.d
@@ -0,0 +1,33 @@
+dependencies: \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworks/math/ofMath.cpp \
+  ../../../../libs/openFrameworks/math/ofMath.h \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/types/ofPoint.h \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/math/ofVec3f.h \
+  ../../../../libs/openFrameworks/math/ofVec2f.h \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
+  ../../../../libs/glew/include/GL/glew.h \
+  ../../../../libs/tess2/include/tesselator.h \
+  ../../../../libs/openFrameworks/math/ofVec4f.h \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofUtils.h \
+  ../../../../libs/openFrameworks/utils/ofLog.h \
+  ../../../../libs/openFrameworks/utils/ofFileUtils.h \
+  ../../../../libs/poco/include/Poco/File.h \
+  ../../../../libs/poco/include/Poco/Foundation.h \
+  ../../../../libs/poco/include/Poco/Config.h \
+  ../../../../libs/poco/include/Poco/Platform.h \
+  ../../../../libs/poco/include/Poco/Platform_POSIX.h \
+  ../../../../libs/poco/include/Poco/Bugcheck.h \
+  ../../../../libs/poco/include/Poco/Types.h \
+  ../../../../libs/poco/include/Poco/Timestamp.h \
+  ../../../../libs/poco/include/Poco/File_UNIX.h \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/types/ofTypes.h \
+  ../../../../libs/openFrameworks/types/ofColor.h \
+  ../../../../libs/poco/include/Poco/Mutex.h \
+  ../../../../libs/poco/include/Poco/Exception.h \
+  ../../../../libs/poco/include/Poco/ScopedLock.h \
+  ../../../../libs/poco/include/Poco/Mutex_POSIX.h \
+  ../../../../libs/poco/include/Poco/Path.h \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworks/app/ofAppRunner.h \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/types/ofRectangle.h \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofNoise.h \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/graphics/ofPolyline.h
---
+++ of_v0.8.4_osx_release/libs/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofQtUtils.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworks/video/ofQtUtils.cpp \
+  ../../../../libs/openFrameworks/video/ofQtUtils.h \
+  /Users/yusuke/projects/AiR-PingPong/of_v0.8.4_osx_release/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
+  ../../../../libs/glew/include/GL/glew.h \
+  ../../../../libs/tess2/include/tesselator.h
