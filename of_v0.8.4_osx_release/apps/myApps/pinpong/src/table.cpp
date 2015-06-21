1983dd1b2f00b2e5d3870404bd74050236a155c3
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2015-06-21 15:29:32 +0900

    Make table class to manage game

---
+++ of_v0.8.4_osx_release/apps/myApps/pinpong/src/table.cpp
@@ -0,0 +1,9 @@
+//
+//  table.cpp
+//  pinpong
+//
+//  Created by Naoyoshi Aikawa on 6/21/15.
+//
+//
+
+#include "table.h"
---
+++ of_v0.8.4_osx_release/apps/myApps/pinpong/src/table.h
@@ -0,0 +1,14 @@
+//
+//  table.h
+//  pinpong
+//
+//  Created by Naoyoshi Aikawa on 6/21/15.
+//
+//
+
+#ifndef __pinpong__table__
+#define __pinpong__table__
+
+#include <stdio.h>
+
+#endif /* defined(__pinpong__table__) */
